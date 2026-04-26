b5b9336c8a71df0d2b6e6c8a27f81e3379ad092b
Toushi8sushi <toshithpu@gmail.com>
2026-04-27 01:27:21 +0530

    made a commit so ishaan can record the audio file from his phone

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,11 +1,11 @@
 FS = 44100             # Sample rate (Hz)
 F_CARRIER = 10000  
-SYMBOL_RATE = 6300     # Symbols per second (Baud)
+SYMBOL_RATE = 6300    # Symbols per second (Baud)
 
 # Calculate how many audio samples represent one symbol
+# 44100 Hz /  Baud = 5 samples per symbol
+# 44100 Hz / 7000 Baud = 6.86 samples per symbol
 # 44100 Hz / 6300 Baud = 7 samples per symbol
-# 48000 Hz / 7000 Baud = 6.86 samples per symbol
-# 48000 Hz / 6800 Baud = 7.05 samples per symbol
 SAMPLES_PER_SYMBOL = FS // SYMBOL_RATE
 
 M=4 #M value for M qam, Mpam etc
@@ -16,6 +16,6 @@ len_zadoff_chu = 353
 u_zadoff_chu = 7
 
 #METHODS
-pulse_methods="SINC" # "SQUARE" or "SINC"
+pulse_method="SINC" # "SQUARE" or "SINC"
 modulation_method="QAM" # "QAM" or "PAM"
 
