40352d0117971f606d1981b420538320bcc60223
Toushi8sushi <toshithpu@gmail.com>
2026-04-27 23:19:57 +0530

    fixed a lot of things an we are now ready to record the audio

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,10 +1,10 @@
-FS = 44100             # Sample rate (Hz)
+FS = 44100            # Sample rate (Hz)
 F_CARRIER = 10000  
-SYMBOL_RATE = 6300   # Symbols per second (Baud)
+SYMBOL_RATE = 6300  # Symbols per second (Baud)
 
 # Calculate how many audio samples represent one symbol
-# 44100 Hz /  Baud = 5 samples per symbol
-# 44100 Hz / 7000 Baud = 6.86 samples per symbol
+# 44100 Hz / 8820 Baud = 5 samples per symbol
+# 44100 Hz / 7350 Baud = 6 samples per symbol
 # 44100 Hz / 6300 Baud = 7 samples per symbol
 SAMPLES_PER_SYMBOL = FS // SYMBOL_RATE
 
@@ -19,3 +19,8 @@ u_zadoff_chu = 7
 pulse_method="SINC" # "SQUARE" or "SINC"
 modulation_method="QAM" # "QAM" or "PAM"
 
+#do i want to read or write to the message.txt file, true to read 
+flag=True
+
+#gap between zadoff and pilots
+SYNC_GAP_SECONDS=0
--- src/sync.py
+++ src/sync.py
@@ -1,23 +1,5 @@
 import numpy as np
 
-def generate_zadoff_chu(u, root_seq_len):
-    """
-    Generates a Zadoff-Chu sequence for time-domain synchronization.
-    
-    Parameters:
-    u (int): Root index. Must be relatively prime to root_seq_len.
-    root_seq_len (int): Length of the sequence (N_zc). Prime numbers (e.g., 63, 127) are best.
-    
-    Returns:
-    np.ndarray: Complex Zadoff-Chu sequence.
-    """
-    n = np.arange(root_seq_len)
-    
-    # Calculate the sequence based on the standard CAZAC formula
-    zc_seq = np.exp(-1j * np.pi * u * n * (n + 1) / root_seq_len)
-    
-    return zc_seq
-
 def append_sync_sequence(data_symbols, sync_sequence):
     """
     Appends the time-domain sync sequence to the start of the transmission.
