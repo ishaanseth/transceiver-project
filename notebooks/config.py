f12a659cd1edb44af65f406989f93f7e569b5ee0
Toushi8sushi <toshithpu@gmail.com>
2026-05-14 20:18:24 +0530

    Toshith: made changes and added a recording of the 16 QAM

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,7 +1,7 @@
 import numpy as np
 FS = 44100            # Sample rate (Hz)
-F_CARRIER = 10000  
-SYMBOL_RATE = 6300  # Symbols per second (Baud)
+F_CARRIER = 12000  
+SYMBOL_RATE = 2000  # Symbols per second (Baud)
 
 # Calculate how many audio samples represent one symbol
 # 44100 Hz / 8820 Baud = 5 samples per symbol
@@ -9,11 +9,11 @@ SYMBOL_RATE = 6300  # Symbols per second (Baud)
 # 44100 Hz / 6300 Baud = 7 samples per symbol
 SAMPLES_PER_SYMBOL = FS // SYMBOL_RATE
 
-M=4 #M value for M qam, Mpam etc. Set M=2 for BPSK.
-total_symbols=50000
+M=4 #M value for M qam, Mpam etc
+total_symbols=10000
 
 #zadoff chu
-len_zadoff_chu = 353
+len_zadoff_chu = 839
 u_zadoff_chu = 7
 
 #METHODS
